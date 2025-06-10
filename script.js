9419fb0dca67659ff6a6c4fc18297dd8bc87b1b0
Gustavo <gustavo_farias29@outlook.com>
2025-06-09 22:15:28 -0300

    Inicio do projeto

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo de interatividade - Alerta simples ao clicar no botão
+document.getElementById('ctaButton').addEventListener('click', function() {
+    alert('Você será redirecionado para o nosso menu!');
+});
